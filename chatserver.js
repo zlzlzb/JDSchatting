82c9d9e2578c1cf35ad84313030a3e333c2c5189
zlzlzb <zlzlzb@nate.com>
2017-06-27 14:18:01 +0900

    JDS chatting Init

---
+++ chatserver.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var express = require('express');
+var socketio = require('socket.io');
+
+var app = express();
+var server = http.createServer(app);
+var io = socketio(server);
+
+var users=[];
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/public/chat.html');
+});
+
+server.listen(3000, function(){
+	console.log('server running port:3000...');
+});
+
+io.on('connection', function(socket){
+	console.log('connected.' + socket.id);
+	socket.on('login', function(data){
+		io.emit('msg',{text : data.id +' has connected.'} );
+		users.push(data.id);
+		io.emit('users',{userlist:users});
+	});
+	socket.on('msg', function(data){
+		io.emit('msg', {text: data.id + ':' + data.text});
+	});
+	socket.on('logout', function(data){
+		io.emit('msg', {text : data.id + ' has disconnected.' });
+		users.pop(data.id);
+		io.emit('users',{userlist:users});
+	});
+});
+
